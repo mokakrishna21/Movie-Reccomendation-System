815d38376bad5e275f2d6da773ab8fbaa6a54068
Moka Krishna <85010044+mokakrishna21@users.noreply.github.com>
2023-09-08 21:12:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,17 +6,14 @@ from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.feature_extraction.text import CountVectorizer
 from tmdbv3api import TMDb, Movie
 
-# Load movie data and create similarity matrix
 movies = pickle.load(open('movie_list.pkl', 'rb'))
 cv = CountVectorizer(max_features=5000, stop_words='english')
 vector = cv.fit_transform(movies['tags']).toarray()
 similarity = cosine_similarity(vector)
 
-# Initialize TMDb API
 tmdb = TMDb()
 tmdb.api_key = "c6ac6f6b45fdf5951c59c02520f63b5c"
 
-# Function to fetch movie poster
 def fetch_poster(movie_id):
     url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=c6ac6f6b45fdf5951c59c02520f63b5c&language=en-US"
     data = requests.get(url)
@@ -25,14 +22,12 @@ def fetch_poster(movie_id):
     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
     return full_path
 
-# Function to fetch cast information
 def fetch_cast_info(movie_id):
     movie_api = Movie()
     credits = movie_api.credits(movie_id)
     cast = credits['cast']
     return cast
 
-# Function to recommend movies
 def recommend(movie, num_recommendations=10):
     index = movies[movies['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
@@ -42,13 +37,11 @@ def recommend(movie, num_recommendations=10):
         recommended_movies.append((movie_id, fetch_poster(movie_id)))
     return recommended_movies
 
-# Function to fetch movie details
 def fetch_movie_details(movie_id):
     movie_api = Movie()
     movie_details = movie_api.details(movie_id)
     return movie_details
 
-# Streamlit app
 st.set_page_config(
     page_title="Poppy's Movie Recommender",
     page_icon=":movie_camera:",
@@ -57,7 +50,6 @@ st.set_page_config(
 
 st.title("Poppy's Movie Recommender")
 
-# Sidebar
 st.sidebar.header("Settings")
 selected_movie = st.sidebar.selectbox("Select a movie:", movies['title'].values)
 num_recommendations = st.sidebar.slider("Number of Recommendations", min_value=1, max_value=20, value=5)
@@ -65,17 +57,14 @@ num_recommendations = st.sidebar.slider("Number of Recommendations", min_value=1
 if st.sidebar.button('Show Recommendations'):
     st.sidebar.info("Fetching recommendations...")
 
-    # Recommendations
     recommended_movies = recommend(selected_movie, num_recommendations)
     
-    # Display recommendations
     st.subheader("Recommended Movies:")
     row = st.beta_columns(5)
     for movie_id, movie_poster in recommended_movies:
         with row.pop():
             st.image(movie_poster, use_column_width=True)
 
-    # Display movie details
     st.subheader("Movie Details:")
     movie_details = fetch_movie_details(recommended_movies[0][0])
     if movie_details:
@@ -86,7 +75,6 @@ if st.sidebar.button('Show Recommendations'):
         st.write(f"**Vote Count:** {movie_details.vote_count}")
         st.write(f"**Genres:** {', '.join([genre.name for genre in movie_details.genres])}")
 
-        # Display cast information
         cast_info = fetch_cast_info(recommended_movies[0][0])
         st.subheader("Cast:")
         for i, cast in enumerate(cast_info[:5]):
