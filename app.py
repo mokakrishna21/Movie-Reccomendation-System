5d1331521cd4b13fd4c6dc80dbcb0be68d2dfdea
Moka Krishna <85010044+mokakrishna21@users.noreply.github.com>
2023-09-08 21:17:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-import pickle
 import streamlit as st
-import requests
 import pandas as pd
-from sklearn.metrics.pairwise import cosine_similarity
-from sklearn.feature_extraction.text import CountVectorizer
-from tmdbv3api import TMDb, Movie
+import numpy as np
 
-# ... (previous code remains unchanged)
+# Sample movie data (replace this with your data loading logic)
+data = {
+    'title': ['Movie 1', 'Movie 2', 'Movie 3', 'Movie 4', 'Movie 5'],
+    'overview': ['Overview 1', 'Overview 2', 'Overview 3', 'Overview 4', 'Overview 5'],
+    'release_date': ['2021-01-01', '2022-02-02', '2023-03-03', '2024-04-04', '2025-05-05'],
+    'vote_average': [7.5, 8.0, 6.5, 7.0, 8.5],
+    'vote_count': [100, 200, 150, 180, 250],
+    'genres': [['Action', 'Adventure'], ['Comedy', 'Drama'], ['Sci-Fi'], ['Horror'], ['Drama', 'Romance']]
+}
 
+movies = pd.DataFrame(data)
+
+# Streamlit app
 st.set_page_config(
     page_title="Poppy's Movie Recommender",
     page_icon=":movie_camera:",
@@ -23,27 +30,26 @@ num_recommendations = st.sidebar.slider("Number of Recommendations", min_value=1
 if st.sidebar.button('Show Recommendations'):
     st.sidebar.info("Fetching recommendations...")
 
-    recommended_movies = recommend(selected_movie, num_recommendations)
+    # Dummy recommendations (replace with your recommendation logic)
+    recommended_movies = np.random.choice(movies['title'], num_recommendations, replace=False)
     
     st.subheader("Recommended Movies:")
-    cols = st.columns(5)  # Use st.columns instead of st.beta_columns
-    for col, (movie_id, movie_poster) in zip(cols, recommended_movies):
+    cols = st.columns(5)
+    for col, movie_title in zip(cols, recommended_movies):
         with col:
-            st.image(movie_poster, use_column_width=True)
+            st.write(f"**{movie_title}**")  # Display recommended movie titles
 
     st.subheader("Movie Details:")
-    movie_details = fetch_movie_details(recommended_movies[0][0])
-    if movie_details:
-        st.write(f"**Title:** {movie_details.title}")
-        st.write(f"**Overview:** {movie_details.overview}")
-        st.write(f"**Release Date:** {movie_details.release_date}")
-        st.write(f"**Average Vote:** {movie_details.vote_average}")
-        st.write(f"**Vote Count:** {movie_details.vote_count}")
-        st.write(f"**Genres:** {', '.join([genre.name for genre in movie_details.genres])}")
+    selected_movie_data = movies[movies['title'] == selected_movie].iloc[0]
+    st.write(f"**Title:** {selected_movie_data['title']}")
+    st.write(f"**Overview:** {selected_movie_data['overview']}")
+    st.write(f"**Release Date:** {selected_movie_data['release_date']}")
+    st.write(f"**Average Vote:** {selected_movie_data['vote_average']}")
+    st.write(f"**Vote Count:** {selected_movie_data['vote_count']}")
+    st.write(f"**Genres:** {', '.join(selected_movie_data['genres'])}")
 
-        cast_info = fetch_cast_info(recommended_movies[0][0])
-        st.subheader("Cast:")
-        for i, cast in enumerate(cast_info[:5]):
-            st.write(f"- {cast['name']} as {cast['character']}")
-    else:
-        st.warning("Movie details not available.")
+    st.subheader("Cast:")
+    # Dummy cast information (replace with your data)
+    cast_info = ['Actor 1 as Character 1', 'Actor 2 as Character 2', 'Actor 3 as Character 3']
+    for cast in cast_info:
+        st.write(f"- {cast}")
